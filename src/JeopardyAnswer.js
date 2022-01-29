3aaa015dc00838a41ae22bb94b311a4aedfdac36
Byron Lutz <byronlutz@gmail.com>
2022-01-29 13:19:59 -0800

    Allow HTML tags from API in answer

--- src/JeopardyAnswer.js
+++ src/JeopardyAnswer.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types'
 function JeopardyAnswer({answer, onRight, onWrong}) {
   const [shown, setShown] = useState(false)
   return <Fragment>
-    <div>{shown ? <h3>{answer}</h3> : <h3 onClick={() => setShown(true)}>(reveal answer)</h3>}</div>
+    <div>{shown ?
+      <h3 dangerouslySetInnerHTML={{__html: answer}}></h3>
+      :
+      <h3 onClick={() => setShown(true)}>(reveal answer)</h3>
+    }</div>
     {shown && <div>
       <a href='#' onClick={onRight}>I got it right! 🎉</a> • <a href='#' onClick={onWrong}>I got it wrong 😭</a>
     </div>}
